9946ed02e33a3669ec5578d3d6fd2cc4aa7ca65e
norielu <nrc41826.nifty.com>
2017-02-25 03:20:08 +0000

    接続と切断のイベント発生時にConsoleに記述する実装他

--- app/entry.js
+++ app/entry.js
@@ -15,9 +15,17 @@ movingButton.click(() => {
   block.animate({ 'marginLeft': '20px' }, 1000);
 });
 
-const loadavg = $('#loadavg');
+const loadavg = $('#loadavg'); // index.jade にあるpタグのid（この場所にappendChild的なことをしている）
 
 const socket = require('socket.io-client')('http://localhost:8000');
 socket.on('server-status', (data) => {
-  loadavg.text(data.loadavg.toString());
+  loadavg.text(data.loadavg.toString() + ', ' + data.uptime); // data.loadavg は配列なのでtoString()で文字列にしているが、そのままでも表示のされ方は同じ
+});
+
+socket.on('connect', () => {
+  console.log('接続しました。');
+});
+
+socket.on('disconnect', () => {
+  console.log('切断しました。');
 });
--- routes/server-status.js
+++ routes/server-status.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const os = require('os');
 
 router.get('/', (req, res, next) => {
-  res.json({ loadavg: os.loadavg() });
+  res.json(
+    { loadavg: os.loadavg(), uptime: os.uptime() } // ここに書いて http://localhost:8000/server-status で os.~ がどんなものか確認する
+    
+  );
 });
 
 module.exports = router;
