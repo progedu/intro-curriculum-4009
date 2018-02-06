aa14fd3ed2814a561c125b67aa78a2eb61c89ba5
gurgur717 <guun888@gmail.com>
2018-02-06 11:22:38 +0000

    接続時、切断時にログを出す

--- app/entry.js
+++ app/entry.js
@@ -21,3 +21,12 @@ const socket = require('socket.io-client')('http://localhost:8000');
 socket.on('server-status', (data) => {
   loadavg.text(data.loadavg.toString());
 });
+
+socket.on('connect', (data) => {
+  console.log('接続しました');
+});
+
+socket.on('disconnct', (data) => {
+  console.log('切断しました');
+});
+
