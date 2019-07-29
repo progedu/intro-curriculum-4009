4d5f6cb0e15bd3e168571e88999743e36143f7c5
Takahiro Yajima <mail@halves.name>
2019-07-29 14:10:00 +0900

    WebSocket接続/切断時にconsole.log

--- app/entry.js
+++ app/entry.js
@@ -22,3 +22,9 @@ const socket = io('http://localhost:8000');
 socket.on('server-status', (data) => {
   loadavg.text(data.loadavg.toString());
 });
+socket.on('connect', () => {
+  console.log('WebSocketに接続しました')
+});
+socket.on('disconnect', () => {
+  console.log('WebSocketから切断しました')
+});
