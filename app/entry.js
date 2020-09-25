f2da14d813c42dc4cf4c1609e412ddc4e45f7356
zawa <re0162vk@gmail.com>
2020-09-25 14:02:43 +0900

    接続、切断のメッセージをコンソールに表示

--- app/entry.js
+++ app/entry.js
@@ -19,6 +19,13 @@ const loadavg = $('#loadavg');
 
 import io from 'socket.io-client';
 const socket = io('http://localhost:8000');
+
 socket.on('server-status', (data) => {
   loadavg.text(data.loadavg.toString());
+});
+socket.on('connect', () => {
+  console.log('接続しました')
+});
+socket.on('disconnect', () => {
+  console.log('切断しました')
 });
