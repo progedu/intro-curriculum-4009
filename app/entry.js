768e69cf8c179ecbff8bcd88fb9075c26cf746a6
Daichi Iida <daichi.iida0919@gmail.com>
2021-01-12 19:44:30 +0900

    クライアントの Chrome のデベロッパーツールの Console に 「接続しました」と「切断しました」という文字列をそれぞれ表示

--- app/entry.js
+++ app/entry.js
@@ -22,3 +22,6 @@ const socket = io('http://localhost:8000');
 socket.on('server-status', (data) => {
   loadavg.text(data.loadavg.toString());
 });
+
+socket.on('connect', () => { console.log('接続しました'); });
+socket.on('disconnect', () => { consaole.log('切断しました'); });
